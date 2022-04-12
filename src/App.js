268021a617a7ba87ada5037a4fadcf72723a179f
Jagroop Singh <jagroopofficial2000@gmail.com>
2022-04-12 21:00:41 +0530

    12 April

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
   return (
     <div>
       <h1 className='heading'>BMI Calculator</h1>
-    <div  className="container">
+    {/* <div  className="container">
       
     <Card className='card-style'>
       <p className='text'>Fill Below Details :</p>
    
     </Card>
      
-    </div>
+    </div> */}
   
     </div>
   );
